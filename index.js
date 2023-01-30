0598787a859ef36b06ebf0a9d04ab00672d9f2a5
MapleDev <67868337+MapleDev423@users.noreply.github.com>
2023-01-31 03:21:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cron = require('./cronservice.js')
 const PORT = process.env.PORT || 5000;
 
 // Run the Server
+// two zero zero seven two zero two
 // Test coin
 //  secondCoin: '0xA3733d4f64365776C2EE3e7Bcec2665B49BEb3e9'
 //  mergeCoin:'0x600bE5FcB9338BC3938e4790EFBeAaa4F77D6893'
